9e911caf30ebe8ebc8dedfb933cdc223443497dc
unknown <amaechinaikechukwu6@gmail.com>
2023-07-11 21:41:59 +0100

    login details package7

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-//@ts-ignore
 const express = require("express");
 const { Request, Response } = require("express");
 
@@ -6,18 +5,20 @@ const { loginDetails } = require("./controllers/GetLoginDetails");
 
 const app = express();
 app.use(express.json());
-app.post("/login", async (req:Request, res:Response) => {
+app.post("/login", async (req: Request, res: Response) => {
   const data = req.body;
   try {
-    if(!data.userId || !data.token){
-    res.status(400).json({error:`You are either missing user id or user token`})
-  }
-   else {const result = await loginDetails(data);
-       res.status(200).json({ message: result });
-   }  } catch (error:any) {
+    if (!data.userId || !data.token) {
+      res.status(400).json({ error: "You are either missing user id or user token" });
+    } else {
+      const result = await loginDetails(data);
+      res.status(200).json({ message: result });
+    }
+  } catch (error) {
+    //@ts-ignore
     res.status(500).json({ error: error.message });
   }
 });
-   app.listen(3000,()=>{
-    console.log('App is ready')
-   });
+app.listen(3000, () => {
+  console.log('App is ready');
+});
