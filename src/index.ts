a031c2bc7a362ee36021b3e6af80ca746df508a0
unknown <amaechinaikechukwu6@gmail.com>
2023-07-12 18:30:10 +0100

    change es_year5

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,20 @@
-const express = require("express");
-
-
-const { loginDetails } = require("./controllers/GetLoginDetails");
-
+//@ts-ignore
+import express , {Request,Response} from "express";
+import { loginDetails } from "./controllers/GetLoginDetails";
 const app = express();
 app.use(express.json());
-app.post("/login", async (req:any, res:any) => {
+app.post("/login", async (req:Request, res:Response) => {
   const data = req.body;
   try {
-    if (!data.userId || !data.token) {
-      res.status(400).json({ error: "You are either missing user id or user token" });
-    } else {
-      const result = await loginDetails(data);
-      res.status(200).json({ message: result });
-    }
-  } catch (error) {
-    //@ts-ignore
+    if(!data.userId || !data.token){
+    res.status(400).json({error:`You are either missing user id or user token`})
+  }
+   else {const result = await loginDetails(data);
+       res.status(200).json({ message: result });
+   }  } catch (error:any) {
     res.status(500).json({ error: error.message });
   }
 });
-app.listen(3000, () => {
-  console.log('App is ready');
-});
+   app.listen(3000,()=>{
+    console.log('App is ready')
+   });
