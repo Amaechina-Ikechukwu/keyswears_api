543d2a010e2f8a2b746ee7f287aafffa6ad90b26
unknown <amaechinaikechukwu6@gmail.com>
2023-07-14 16:09:56 +0100

    will update token if user exists

--- src/controllers/GetLoginDetails.ts
+++ src/controllers/GetLoginDetails.ts
@@ -1,54 +1,56 @@
-import { createClient } from "@supabase/supabase-js";
-import 'dotenv/config';
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Provide a custom schema. Defaults to "public".
-const supabase = createClient(
-  process.env.SUPABASE_URL ?? "",
-  process.env.SUPABASE_KEY ?? ""
-);
-const registerNewUser =async (data)=>{
-   const { error: insertRowError } = await supabase.from('logindetails').insert([
-    {
-      userid: data.userId,
-      token: data.token,
-    },
-  ]).select();
+const supabaseUrl: string = process.env.SUPABASE_URL || "";
+const supabaseKey: string = process.env.SUPABASE_KEY || "";
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
+
+const registerNewUser = async (data: { userId: string; token: string }): Promise<string> => {
+  const { error: insertRowError } = await supabase
+    .from('logindetails')
+    .insert([
+      {
+        userid: data.userId,
+        token: data.token,
+      },
+    ]);
 
   if (insertRowError) {
     throw new Error(`There seems to be an error: ${insertRowError.message}`);
   } else {
     return "Done";
   }
-}
+};
 
-const updateToken =async()=> {
-  const {data:updatedData,error:updateError}= await supabase
-  .from('logindetails').update({token :data.token}).eq('userid',data.userId);
-  if(updateError){
-    throw new Error ( `Error updating username: ${updateError.message}` )
-  }
-  else
+const updateToken = async (data: { userId: string; token: string }): Promise<string> => {
+  const { error: updateError } = await supabase
+    .from('logindetails')
+    .update({ token: data.token })
+    .eq('userid', data.userId);
 
-  {
-    return done
+  if (updateError) {
+    throw new Error(`Error updating token: ${updateError.message}`);
+  } else {
+    return "Done";
   }
-}
-export async function loginDetails(data: {
-  userId: string;
-  token: string;
-}): Promise<string> {
- const Data = data;
- const {data, error}= await supabase.from('logindetails').select('*').eq('userid',data.userId)
- if(error){
-  throw new Error(`Error fetching user: ${error.message}`)
- }
- else
- {
-  if (data.length == 0){
-    registerNewUser(Data)
+};
+
+export async function loginDetails(data: { userId: string; token: string }): Promise<void> {
+  const { data: userData, error } = await supabase
+    .from('logindetails')
+    .select('*')
+    .eq('userid', data.userId);
+
+  if (error) {
+    throw new Error(`Error fetching user: ${error.message}`);
+  } else {
+    if (userData.length === 0) {
+      await registerNewUser(data);
+    } else {
+      await updateToken(data);
+    }
   }
-  else{
-    updateToken(Data)
-  }
- }
 }
