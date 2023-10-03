c218f67e86aac6808b587265ac639a3793dcc5be
unknown <amaechinaikechukwu6@gmail.com>
2023-10-03 13:08:11 +0100

    old cors code

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,38 @@
+//@ts-ignore
 import express, { Request, Response, NextFunction } from "express";
 import pagesWebhookRouter from "./webhooks/PageWebhook";
 import "dotenv/config";
-
 const app = express();
-const port = process.env.PORT || 3000;
-
+import pagesRouter from "./middlewares/Routing/pageroutes";
+import igRouter from "./middlewares/Routing/instagramroutes";
 app.use(express.json());
-
-// Enable CORS to allow all origins, methods, and headers
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // Allow specified HTTP methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Allow specified headers
+  const allowedOrigins = [
+    "https://chooyagroup.com",
+    "https://496a-197-211-52-68.ngrok-free.app",
+  ];
+  const origin: string | undefined = req.headers.origin;
+
+  // Check if the request origin is in the allowed origins list
+  if (origin && allowedOrigins.includes(origin)) {
+    // Allow the specific origin
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  } else {
+    // For any other origin (except localhost), allow it
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
 ////////////////////////////////
 app.use("/", pagesWebhookRouter);
-// Add your other middleware and routes here
+app.use("/", pagesRouter);
+app.use("/instagram", igRouter);
 ////////////////////////////////
 
-app.listen(port, () => {
+app.listen(3003, () => {
   console.log("App is ready");
 });
