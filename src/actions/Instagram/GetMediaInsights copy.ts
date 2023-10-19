620f0114b0260bc81909f856eceace1948087346
unknown <amaechinaikechukwu6@gmail.com>
2023-10-19 18:44:41 +0100

    fixed comment route

--- src/actions/Instagram/GetMediaInsights copy.ts
+++ src/actions/Instagram/GetMediaInsights copy.ts
@@ -6,7 +6,7 @@ export default async function GetMediaInsight(
   token: string
 ): Promise<string> {
   try {
-    const url = `https://graph.facebook.com/${media_id}/insights?metric=total_interactions,impressions,reach,saved,video_views&access_token=${token}`;
+    const url = `https://graph.facebook.com/${media_id}/insights?metric=impressions,reach,carousel_album_impressions,carousel_album_reach,carousel_album_engagement,carousel_album_saved,carousel_album_video_views,taps_forward,taps_back,exits,replies,engagement,saved,video_views&period=day&access_token=${token}`;
 
     const response = await axios.get(url);
     const result = response.data;
